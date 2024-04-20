9f256b1be8d441ea013388f59bbe2264f65d122a
Giulio Zanchetta <zank@outlook.it>
2024-04-20 15:30:23 +0200

    Refactor component to extend gsap timeline instead of using proxies

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# gsap-timeline-pooler
+# Astro gsap
+
+
 
--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-export * from "./src/pooler";
+import gsap from "gsap";
+
+export const pool = new Set<gsap.core.Timeline>();
+
+document.addEventListener("astro:before-swap", (ev) => {
+	// before swapping with a new page clean all the timelines
+	for (const timeline of pool.values()) {
+		timeline.kill();
+	}
+	pool.clear();
+});
+
+/**
+ * Add the timeline to the pool so the pool can be used later on
+ * to kill all the timelines so they can be garbage collected
+ */
+class TimelinesRecycler extends gsap.core.Timeline {
+	constructor(vars?: gsap.TimelineVars, time?: number) {
+		super(vars, time);
+		pool.add(this);
+	}
+}
+
+const gsapExt = {
+	...gsap,
+	timeline: (vars?: gsap.TimelineVars) => {
+		return new TimelinesRecycler(vars);
+	},
+};
+
+export default gsapExt;
